e520ece2319edf1bcd2cf018e99e5a35a84b887a
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-10-16 15:55:34 +0300

    Создал header.php и footer.php, включил показ ошибок, подключил добавление пользовательского логотипа, сделал динамический title, создали области меню, реализовали меню в шапке с помощь walker bootstrap 4

--- band-digital/functions.php
+++ band-digital/functions.php
@@ -1,4 +1,29 @@
 <?php 
+
+// Есть ли эта функция, если нет то создаем её
+//(не запускалась ли такая функция до этого)
+
+if(! function_exists('band_digital_setup')){
+    function band_digital_setup() {
+// Создаем  пользовательский логотип
+        add_theme_support('custom-logo', [
+            // смотрим какой у нас логотип по высоте ширине
+            'height'      => 50,
+            'width'       => 130,
+            'flex-width'  => false,
+            'flex-height' => false,
+            'header-text' => '',
+            'unlink-homepage-logo' => false, // WP 5.5
+        ]);
+        //Добавляем динамический <title>
+        add_theme_support( 'title-tag');
+    }
+    add_action('after_setup_theme', 'band_digital_setup');
+}
+
+
+
+
 /*
 Подключение стилей и скриптов
 */
@@ -37,12 +62,91 @@ function band_digital_scripts(){
     wp_enqueue_script( 'contact', get_template_directory_uri() . '/plugins/jquery/contact.js', array('jquery'), null, true);
     wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array('jquery'), null, true);
 
-
-
-
-
-
-
 }
+
+// Регистрируем несколько меню
+
+function band_digital_menus(){
+    // собираем несколько зон (областей) меню
+    $locations = array(
+        'header' => __('Header Menu', 'band_digital'),
+        'footer' => __('Footer Menu', 'band_digital'),
+    );
+    // регистрируем области меню, которые лежат в переменной $locations
+    register_nav_menus($locations);
+}
+// хук-событие
+add_action('init','band_digital_menus');
+
+// // добавим класс nav-item ко всем пунктам меню
+// add_filter('nav_menu_css_class','custom_nav_menu_css_class', 10,1);
+// // получаем список всех классов пунктов меню
+// function custom_nav_menu_css_class($classes){
+//     // добавляем к списку классов свой класс nav-item
+//     $classes[] = 'nav-item';
+//     // возвращаем список классов уже с нашим классом
+//     return $classes;
+// }
+
+// // повесим на все ссылки класс nav-link
+// add_filter('nav_menu_link_attributes','custom_nav_menu_link_attributes', 10);
+// function custom_nav_menu_link_attributes($atts){
+//     $atts['class'] = 'nav-link';
+//     return $atts;
+// }
+
+
+class bootstrap_4_walker_nav_menu extends Walker_Nav_menu {
+    
+    function start_lvl( &$output, $depth = 0, $args = array()  ){ // ul
+        $indent = str_repeat("\t",$depth); // indents the outputted HTML
+        $submenu = ($depth > 0) ? ' sub-menu' : '';
+        $output .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
+    }
+  
+  function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ){ // li a span
+        
+    $indent = ( $depth ) ? str_repeat("\t",$depth) : '';
+    
+    $li_attributes = '';
+        $class_names = $value = '';
+    
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        
+        $classes[] = ($args->walker->has_children) ? 'dropdown' : '';
+        $classes[] = ($item->current || $item->current_item_anchestor) ? 'active' : '';
+        $classes[] = 'nav-item';
+        $classes[] = 'nav-item-' . $item->ID;
+        if( $depth && $args->walker->has_children ){
+            $classes[] = 'dropdown-menu';
+        }
+        
+        $class_names =  join(' ', apply_filters('nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+        $class_names = ' class="' . esc_attr($class_names) . '"';
+        
+        $id = apply_filters('nav_menu_item_id', 'menu-item-'.$item->ID, $item, $args);
+        $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+        
+        $output .= $indent . '<li ' . $id . $value . $class_names . $li_attributes . '>';
+        
+        $attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+        $attributes .= ! empty( $item->target ) ? ' target="' . esc_attr($item->target) . '"' : '';
+        $attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
+        $attributes .= ! empty( $item->url ) ? ' href="' . esc_attr($item->url) . '"' : '';
+        
+        $attributes .= ( $args->walker->has_children ) ? ' class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="nav-link"';
+        
+        $item_output = $args->before;
+        $item_output .= ( $depth > 0 ) ? '<a class="dropdown-item"' . $attributes . '>' : '<a' . $attributes . '>';
+        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+        
+        $output .= apply_filters ( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    
+    }
+    
+}
+
 
 ?>
