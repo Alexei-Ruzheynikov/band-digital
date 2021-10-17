803229e85a0b8b9f4bd6696036eafcf00cbcc1d1
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-10-17 17:49:41 +0300

    Сделал пагинацию для записей в home.php

--- band-digital/functions.php
+++ band-digital/functions.php
@@ -19,7 +19,7 @@ if(! function_exists('band_digital_setup')){
         add_theme_support( 'title-tag');
         // Подключаем миниатюры для постов и страниц
         add_theme_support( 'post-thumbnails' );
-        set_post_thumbnail_size( 730, 480 ); // размер миниатюры поста по умолчанию
+        set_post_thumbnail_size( 730, 480, true ); // размер миниатюры поста по умолчанию
     }
     add_action('after_setup_theme', 'band_digital_setup');
 }
@@ -163,5 +163,28 @@ function delete_intermediate_image_sizes( $sizes ){
 	] );
 }
 
+
+// удаляет H2 из шаблона пагинации
+add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
+function my_navigation_template( $template, $class ){
+	/*
+	Вид базового шаблона:
+	<nav class="navigation %1$s" role="navigation">
+		<h2 class="screen-reader-text">%2$s</h2>
+		<div class="nav-links">%3$s</div>
+	</nav>
+	*/
+
+	return '
+	<nav class="navigation %1$s" role="navigation">
+		<div class="nav-links">%3$s</div>
+	</nav>    
+	';
+}
+
+// выводим пагинацию
+the_posts_pagination( array(
+	'end_size' => 2,
+) ); 
 
 ?>
