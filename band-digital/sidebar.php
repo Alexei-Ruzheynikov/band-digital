cad9fc2775fc0cd4a418f7e71a12cfdf1ef05471
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-10-18 11:13:10 +0300

    Создал sidebar.php; Настроил виджеты рубрик и тэгов; Создал свой виджет - "загрузка файлов"

---
+++ band-digital/sidebar.php
@@ -0,0 +1,48 @@
+            <aside class="col-lg-4">
+<div class="sidebar-widget search">
+    <?php get_search_form(); ?>
+</div>
+    <?php if (! dynamic_sidebar('sidebar-blog') ) : ?>
+    <?php dynamic_sidebar( 'sidebar-blog' ); ?>
+    <?php endif; ?>
+                        
+
+                        
+                        <!-- <div class="col-lg-12">
+                            <div class="sidebar-widget category">
+                                <h5 class="mb-3">Рубрики</h5>
+                                <ul class="list-styled">
+                                    <li>Маркетинг</li>
+                                    <li>Диджитал</li>
+                                    <li>SEO</li>
+                                    <li>Веб-дизайн</li>
+                                    <li>Разработка</li>
+                                    <li>Видео</li>
+                                    <li>Подкаст</li>
+                                </ul>
+                            </div>
+                        </div> -->
+
+                        <!-- <div class="col-lg-12">
+                            <div class="sidebar-widget tag">
+                                <a href="#">web</a>
+                                <a href="#">development</a>
+                                <a href="#">seo</a>
+                                <a href="#">marketing</a>
+                                <a href="#">branding</a>
+                                <a href="#">web deisgn</a>
+                                <a href="#">Tutorial</a>
+                                <a href="#">Tips</a>
+                                <a href="#">Design trend</a>
+                            </div>
+                        </div> -->
+                        <!-- <div class="col-lg-12">
+                            <div class="sidebar-widget download">
+                                <h5 class="mb-4">Полезные файлы</h5>
+                                <a href="#"> <i class="fa fa-file-pdf"></i>Презентация Promodise</a>
+                                <a href="#"> <i class="fa fa-file-pdf"></i>10 источников бесплатного SEO</a>
+                            </div>
+                        </div> -->
+
+
+    </aside>   
