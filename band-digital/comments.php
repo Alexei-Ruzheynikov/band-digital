da072f721d73427bca15790d4d26878de268436b
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-10-18 16:53:41 +0300

    Разработал форму отправки комментария в comments.php

--- band-digital/comments.php
+++ band-digital/comments.php
@@ -44,7 +44,49 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-	comment_form();
+
+
+	$defaults = [
+	'fields'               => [
+        'author' => '<div class="row"><div class="col-lg-6">
+			<input class="form-control" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" placeholder="Имя" />
+		</div>',
+		'email'  => '<div class="col-lg-6">
+			<input id="email" name="email" class="form-control" type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" aria-describedby="email-notes" placeholder="E-mail" />
+		</div></div>',
+	],
+	'comment_field'        => '<div class="comment-form-comment mb-3">
+		<textarea class="form-control" id="comment" name="comment" cols="45" rows="8"  aria-required="true" required="required" placeholder="Комментарий"></textarea>
+	</div>',
+	'must_log_in'          => '<p class="must-log-in">' .
+		 sprintf( __( 'Вам нужно <a href="%s">войти</a>, чтобы оставить комментарий.' ), wp_login_url( apply_filters( 'the_permalink', get_permalink( $post->id ) ) ) ) . '
+	 </p>',
+	'logged_in_as'         => '<p class="logged-in-as">' .
+		 sprintf( __( '<a href="%1$s" aria-label="Вы вошли как %2$s.">Вы вошли как %2$s</a>. <a href="%3$s">Выйти?</a>' ), get_edit_user_link(), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( $post->id ) ) ) ) . '
+	 </p>',
+	'comment_notes_before' => '<p class="comment-notes">
+		<span id="email-notes">' . __( 'Ваш E-mail защищен от спама.' ) . '</span>
+	</p>',
+	'comment_notes_after'  => '',
+	'id_form'              => 'commentform',
+	'id_submit'            => 'submit',
+	'class_form'           => 'comment-form',
+	'class_submit'         => 'btn btn-hero btn-circled',
+	'name_submit'          => 'submit',
+	'title_reply'          => __( 'Оставьте комментарий' ),
+	'title_reply_to'       => __( 'Ответить %s' ),
+	'title_reply_before'   => '<h3 id="reply-title" class="comment-reply-title">',
+	'title_reply_after'    => '</h3>',
+	'cancel_reply_before'  => ' <small>',
+	'cancel_reply_after'   => '</small>',
+	'cancel_reply_link'    => __( 'Отменить отправку' ),
+	'label_submit'         => __( 'Отправить комментарий' ),
+	'submit_button'        => '<button name="%1$s" type="submit" id="%2$s" class="%3$s" >%4$s</button>',
+	'submit_field'         => '<p class="form-submit">%1$s %2$s</p>',
+	'format'               => 'html5',
+];
+
+comment_form( $defaults );
 	?>
 
 </div><!-- #comments -->
@@ -87,7 +129,7 @@ if ( post_password_required() ) {
                                 </div> -->
                             </div>
 
-                            <div class="mt-5 mb-3">
+                            <!-- <div class="mt-5 mb-3">
                                 <h3 class="mt-5 mb-2">Оставьте комментарий</h3>
                                 <p class="mb-4">Ваш E-mail защищен от спама</p>
                                 <form action="#" class="row">
@@ -112,4 +154,4 @@ if ( post_password_required() ) {
                                         <a href="#" class="btn btn-hero btn-circled">Оставить комментарий</a>
                                     </div>
                                 </form>
-                            </div>
+                            </div> -->
